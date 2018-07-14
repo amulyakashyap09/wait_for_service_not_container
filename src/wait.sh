04cf846e7b6497e240c36733a42b95deebd9dacd
Amulya Kashyap <amulyakashyap09@gmail.com>
2018-07-14 12:52:49 +0530

    created wait.sh
    created wait.sh

---
+++ src/wait.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+declare -a HOSTS=("localhost:28015" "localhost:9200" "localhost:5601" "localhost:15672")
+
+pingNow(){
+
+    VAL=${HOSTS[$1]}
+    SERVER=$(echo $VAL | awk -F: '{print $1}')
+    PORT=$(echo $VAL | awk -F: '{print $2}')
+
+    if [ $1 -lt `expr $2 - 1` ]; then
+
+        $(nc -w 2 -v $SERVER $PORT </dev/null)
+        
+        if [ "$?" -ne 0 ]; then
+            echo "Connection to $SERVER on port $PORT failed"
+            sleep 2
+            pingNow $1 $2
+            # exit 1
+        else
+            echo "Connection to $SERVER on port $PORT succeeded"
+            pingNow `expr $1 + 1` $2
+            # exit 0
+        fi
+    else
+        echo "DONE!!! All services are UP"
+        ./app
+    fi
+}
+
+pingNow 0 ${#HOSTS[@]}
